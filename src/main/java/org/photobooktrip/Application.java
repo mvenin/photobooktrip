fc97a73d83974ae8277d554c50be3998a10841e0
veninma <marian.venin@gmail.com>
2014-10-07 16:45:46 +0200

    initial commit

--- src/main/java/org/photobooktrip/Application.java
+++ src/main/java/org/photobooktrip/Application.java
@@ -1,17 +1,19 @@
 package org.photobooktrip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan
-@EnableJpaRepositories
-@Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
+@ComponentScan
+//@EnableJpaRepositories
+//@Import(RepositoryRestMvcConfiguration.class)
+@Import(WebMvcAutoConfiguration.class)
+//@EnableWebMvc
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/org/photobooktrip/WebInitializer.java
+++ src/main/java/org/photobooktrip/WebInitializer.java
@@ -1,12 +1,13 @@
 package org.photobooktrip;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class WebInitializer extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 }
